90798c33249a7ccd2618bcf5a6ca5b6c474fdf5a
Daniel Stahl <daniel.stahl@regions.com>
2017-07-02 18:07:23 -0400

    automated compiling

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ Accounts will be automatically generated when accessing the Ivy playground `http
 # Start docker
 
 `docker run -it -p 1999:1999 chaincore/developer:ivy-latest`
+
+# Note on integration test script
+This uses the ivy branch of the chain github branch.  It is not available as part of the npm module.  To use the node from the ivy branch, clone the chain repo:
+
+`git clone https://github.com/chain/chain`
+
+Then checkout the `ivy` branch:
+
+`git checkout ivy`
+
+Then install the packages required:
+
+`cd chain/sdk/node` 
+
+`npm install`
+
--- integrationTest.js
+++ integrationTest.js
@@ -1,9 +1,18 @@
-const chain = require('chain-sdk')
+const chain = require('../chain/sdk/node/src/index')
 const access=require('./client_key.json')
 const accessKey=`client:${access.client}`
-
+const fs=require('fs')
 
 const client = new chain.Client("http://localhost:1999", accessKey)
 
-
+//console.log(client)
 /**I want to put something here where I interact with n depositors and m banks and assess the useability of a deposit/loan relationship.   */
+
+
+/**compile contracts */
+fs.readFile("./DepositContract.ivy", (err, data)=>{
+    err?console.log(err):null
+    client.ivy.compile({
+        contract:data.toString()
+    }).then(result=>console.log(result)).catch(err=>console.log(err))
+})
