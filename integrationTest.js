c2668a45828bcce1c0adb1cc7f1639fa40be7077
Daniel Stahl <daniel.stahl@regions.com>
2017-07-07 12:11:37 -0500

    improved client_key

--- integrationTest.js
+++ integrationTest.js
@@ -1,9 +1,9 @@
 const chain = require('../chain/sdk/node/src/index')
 const access=require('./client_key.json')
-const accessKey=`client:${access.client}`
+const accessKey=`${access.tokenname}:${access.tokenname}`
 const fs=require('fs')
 
-const client = new chain.Client("http://localhost:1999", accessKey)
+const client = new chain.Client(access.url, accessKey)
 
 //console.log(client)
 /**I want to put something here where I interact with n depositors and m banks and assess the useability of a deposit/loan relationship.   */
